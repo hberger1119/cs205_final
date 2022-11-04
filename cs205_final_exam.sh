0e7ed8e9535245598c67bcdfefcee7c5f585f056
hberger <hberger1119@gmail.com>
2022-11-03 19:05:41 -0700

    s is a commit that adds the code and image

--- cs205_final_exam.sh
+++ cs205_final_exam.sh
@@ -1,10 +1,37 @@
-# TODO: Modify this file to create a shell script that is able to use awk to go through a file formatted like pokemon.dat and provides a printed report in the following format (where your script correctly calculates the values that go into the [VALUE] placeholders):
+# TODO: Modify this file to create a shell script that is able to use awk to go through a file formatted like pokemon.dat and provides a printed report in the following format 
+(where your script correctly calculates the values that go into the [VALUE] placeholders):
 # ===== SUMMARY OF DATA FILE =====
 #    File name: [VALUE]
 #    Total Pokemon: [VALUE]
 #    Avg. HP: [VALUE]
 #    Avg. Attack: [VALUE]
 # ===== END SUMMARY =====
+
+import sys
+
+main ():
+    fileobj=open(sys.argv[1])
+    lines=[]
+    new_lines=[]
+
+    avg_hp = 0
+    avg_attack = 0
+
+    for line in fileobj:
+        lines.append(line.strip().split('\t'))
+
+    for pokemon in range (1, len(lines)):
+        hp = int(lines[pokemon][4])
+        attack = int(lines[pokemon][6])
+        avg_hp += hp
+        avg_attack += attack
+        
+    print ("filename: ", fileobj)
+    print ("Total Pokemon :", lines[-1][0])
+    print ("Average HP:", int(avg_hp/(len(lines))))
+    print ("Avg Attack:", int(avg_attack/(len(lines))))
+
+    fileobj.close()
 
 # The "Avg." values should be calculated as mean values for the corresponding columns.
 # The spacing and header formatting should match the above formatting description exactly.
